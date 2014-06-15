6d34c45b38c386dc39980cb86d1f7740f56d17dc
Jen Gilbert <jgilbert@arubanetworks.com>
2014-06-14 22:45:26 -0700

    Sketch in carousel

--- js/base.js
+++ js/base.js
@@ -4,3 +4,17 @@ function mainCtrl($scope) {
     $scope.test = "Angular is working!";
 }
 
+kivaApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '../templates/home.html'
+        }).
+        when('/browse', {
+            templateUrl: '../templates/browse.html'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }
+]);
+
---
+++ js/browseCtrl.js
@@ -0,0 +1,3 @@
+function browseCtrl($scope) {
+    $scope.items = ["red", "green", "blue", "black", "pink", "orange", "taupe", "beige", "white"];
+};
