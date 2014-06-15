b7080d255785a74d16a5012fe9c61233f0a36eba
Rena <alittlemama@gmail.com>
2014-06-15 07:24:09 -0700

    Initial OAuth stuff

--- hacksummit/__init__.py
+++ hacksummit/__init__.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for, session, request
 from flask.ext.heroku import Heroku
+from flask_oauthlib.client import OAuth
 
 from database import db
 from models import Loan, Lender
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
 app.config.from_object('hacksummit.default_config')
 
 heroku = Heroku(app)
@@ -33,7 +36,12 @@ def all_lenders():
   result = ""
 
   for lender in Lender.query.all():
-    result += "<p>%s</p>" % lender.username
+    result += "<p>username=%s" % lender.username
+    result += " : facebook_id=%s" % lender.facebook_id
+    result += " : location=%s" % lender.location
+    result += " : hometown=%s" % lender.hometown
+    result += " : work=%s" % lender.work
+    result += "</p>"
 
   return result
 
@@ -52,3 +60,88 @@ def lender(username):
   user.save()
 
   return "hi %s, you have been created" % user.username
+
+############################################
+
+FACEBOOK_APP_ID = '524414764331163'
+FACEBOOK_APP_SECRET = '5cc64ca9891c35fd651a7fea6d32c0ea'
+
+oauth = OAuth(app)
+facebook = oauth.remote_app('facebook',
+  base_url='https://graph.facebook.com/',
+  request_token_url=None,
+  access_token_url='/oauth/access_token',
+  authorize_url='https://www.facebook.com/dialog/oauth',
+  consumer_key=FACEBOOK_APP_ID,
+  consumer_secret=FACEBOOK_APP_SECRET,
+  request_token_params={'scope': 'email,user_location,user_hometown,user_friends,user_work_history'}
+)
+
+
+@app.route('/update_external_user_data')
+def index():
+  return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+  return facebook.authorize(callback=url_for('facebook_authorized',
+    next=request.args.get('next') or request.referrer or None,
+    _external=True))
+
+
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+  if resp is None:
+    return 'Access denied: reason=%s error=%s' % (
+      request.args['error_reason'],
+      request.args['error_description']
+    )
+  session['oauth_token'] = (resp['access_token'], '')
+
+  me = facebook.get('/me')
+  facebook_id = "%s" % me.data['id']
+
+  user = db.session.query(Lender).filter(Lender.facebook_id==facebook_id).first()
+
+  # If user doesn't exist, create him/her/it
+  created = False
+  if not user:
+    user = Lender()
+    created = True
+
+  # Populate from user attributes
+  user.facebook_id = facebook_id
+  try:
+    # only most frequent for ease of use during hackathon
+    user.work = me.data['work'][0]['position']['name']
+  except KeyError:
+    pass
+  try:
+    user.location = me.data['location']['name']
+  except KeyError:
+    pass
+  try:
+    user.hometown = me.data['hometown']['name']
+  except KeyError:
+    pass
+
+  # Populate from friends
+  myfriends = facebook.get('/me/friends')
+
+  user.save()
+
+  # ???
+  # g.user = user
+
+  action = "created" if created else "updated"
+  return "facebook_id %s %s" % (facebook_id, action)
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+  return session.get('oauth_token')
+
+if __name__ == '__main__':
+  app.run()
